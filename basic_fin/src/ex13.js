ead2f8913dc2d866228d862b780dccde0a834a83
heonwoochoo <cngjsdn9@naver.com>
2022-09-17 17:43:52 +0900

    rocation scale position

---
+++ basic_fin/src/ex13.js
@@ -0,0 +1,68 @@
+import * as THREE from "three";
+import { Vector3 } from "three";
+// ----- 주제: scale
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 5;
+  camera.position.y = 1;
+  scene.add(camera);
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 0;
+  directionalLight.position.z = 10;
+  scene.add(directionalLight);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "red",
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = 2;
+  scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+  function draw() {
+    const time = clock.getElapsedTime();
+    mesh.rotateY(time * 0.001);
+    mesh.scale.set(1, 0.5, 2); // x,y,z 한번에 조절 가능함
+    renderer.render(scene, camera);
+    // window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
